38ab5fde6c2e786724d24c296c7d00d44bc6f71d
Seu Nome <seu_email@example.com>
2024-04-07 15:31:31 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-lzdn.onrender.com")
     websocket.onmessage = processMessage
 }
 
@@ -98,8 +98,8 @@ const sendMessage = (event) => {
     }
 
     websocket.send(JSON.stringify(message))
-
-    chatInput.value = ""
+     
+chatInput.value = ""
 }
 
 loginForm.addEventListener("submit", handleLogin)
